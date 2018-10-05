5b34bdbdd0255cc52eb2fac8bd7254c5cccac6b2
gtauro <gtauro@nd.edu>
2018-10-05 09:07:31 -0400

    Finished exercise 2, e2.sh

---
+++ e2.sh
@@ -0,0 +1,25 @@
+# usage bash e2.sh wages.csv
+# To be used to return gender (-f 1), yearsExperience (-f 2), and wage (-f 4) for top and bottom earners by wage
+# Also returns number of "females" in top 10 earners by wage
+
+list=$(cat "$1" | grep -v "gender" | cut -d , -f 1,2,4 | sort -nr -t , -k 3)
+
+# Outputs top earner
+
+echo "The top earner had the following gender, years of experience, and wage:"
+echo "$list" | head -n 1
+
+# Outputs bottom earner
+
+echo "The bottom earner had the following gender, years of experience, and wage:"
+echo "$list" | tail -n 1
+
+# Lists the number of females in the top 10 earners
+f=$(echo "$list" | head -n 10 | grep "female" | wc -l)
+
+if [ $f -eq 1 ]
+then
+echo "There was $f female in the top 10 earners by wage."
+else
+echo "There were $f females in the top 10 earners by wage."
+fi
